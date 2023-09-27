7a3a110a9be9ef7c319c6e008cbf212867b00496
naveen61346134 <66348947+naveen61346134@users.noreply.github.com>
2023-09-28 01:02:46 +0530

    Update and rename esrgan.py to main.py

---
+++ Real esrgan/main.py
@@ -0,0 +1,66 @@
+import os
+import wget
+import time
+import replicate
+
+with open("token.txt", "r") as t:
+    token = t.readlines()
+os.environ["REPLICATE_API_TOKEN"] = f"{token[0]}"
+dir_path = os.getcwd()
+
+
+def esrgan():
+    file_path = input("Enter complete file path: ")
+    out_file_name = input("Enter output file name: ")
+    if os.path.exists(file_path):
+        pass
+    else:
+        print("File doesnt exist!")
+        exit(1)
+
+    print("Processing...")
+    output = replicate.run(
+        "nightmareai/real-esrgan:42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b",
+        input={"image": open(file_path, "rb"), "face_enhance": True}
+    )
+    out_f = dir_path + f"\\{out_file_name}.jpeg"
+    wget.download(str(output), out=out_f)
+    if os.path.exists(f"{out_file_name}.jpeg"):
+        print("\nFile Created!")
+    else:
+        print("\nERROR")
+    time.sleep(2)
+
+
+def sdxl():
+    prompt = input("Enter prompt for image: ")
+    out_file_name = input("Enter output file name: ")
+    width = input("Enter width: ")
+    height = input("Enter hieight: ")
+    output = replicate.run(
+        "stability-ai/sdxl:8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f",
+        input={"prompt": f"{prompt}", "width": f"{width}", "height": f"{height}", "apply_watermark": False}
+    )
+    out_f = dir_path + f"\\{out_file_name}.jpeg"
+    wget.download(str(output), out=out_f)
+    if os.path.exists(f"{out_file_name}.jpeg"):
+        print("\nFile created!")
+    else:
+        print("\nERROR")
+    time.sleep(2)
+
+if __name__ == "__main__":
+    while True:
+        print("""\n
+            [1] Image upscaler (REAL ESRGAN)
+            [2] Text to image (SDXL)
+        """)
+        choice = input("\nEnter choice: ")
+        if choice == "1":
+            esrgan()
+        elif choice == "2":
+            sdxl()
+        elif choice == "0":
+            exit(0)
+        else:
+            print("Invalid Choice\n")
