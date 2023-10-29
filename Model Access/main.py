c13eba5f8681566003c983287089b1e1f8a66ad1
Naveen William <66348947+naveen61346134@users.noreply.github.com>
2023-10-29 13:46:05 +0530

    improved user interface (main.py)

--- Model Access/main.py
+++ Model Access/main.py
@@ -2,17 +2,25 @@ import os
 import wget
 import time
 import replicate
+from colorama import Fore, Style
 
 try:
     with open("token.txt", "r") as t:
-        token = t.readlines()
-    os.environ["REPLICATE_API_TOKEN"] = f"{token[0]}"
+        token = t.readline()
+    if len(token) <= 10:
+        print(Fore.RED + "INVALID TOKEN")
+        time.sleep(2)
+        exit(1)
+    else:
+        os.environ["REPLICATE_API_TOKEN"] = f"{token}"
 except FileNotFoundError:
-    print("TOKEN FILE NOT FOUND!")
+    print(Fore.RED + "TOKEN FILE NOT FOUND!")
     time.sleep(1)
     exit(1)
 dir_path = os.getcwd()
-os.system("mkdir Output")
+if not os.path.exists("Output"):
+    os.system("mkdir Output")
+    print(Fore.CYAN + "Output folder has been created!")
 
 
 def download(url, out_f, out_file_name, png_check):
@@ -24,14 +32,18 @@ def download(url, out_f, out_file_name, png_check):
     exist = dir_path+file_ext
     if os.path.exists(exist):
         print("\nFile Created!")
+        time.sleep(1)
+        os.system("cls")
     else:
         print("\nERROR")
+        time.sleep(1)
+        os.system("cls")
     time.sleep(2)
 
 
 def file_path_and_check(png):
-    file_path = input("Enter complete file path: ")
-    out_file_name = input("Enter output file name: ")
+    file_path = input(Fore.BLUE + "Enter complete file path: " + Fore.CYAN)
+    out_file_name = input(Fore.BLUE + "Enter output file name: " + Fore.CYAN)
     if os.path.exists(file_path):
         pass
     else:
@@ -47,11 +59,12 @@ def file_path_and_check(png):
 def esrgan():
     file_path, out_f, out_file_name = file_path_and_check(True)
     try:
-        upscale = int(input("Enter upscale value: "))
+        upscale = int(
+            input(Fore.BLUE + "\t\tEnter upscale value: " + Fore.CYAN))
     except ValueError:
         upscale = 4
 
-    print("Processing...")
+    print(Fore.BLUE + "\t\tProcessing...")
     try:
         output = replicate.run(
             "nightmareai/real-esrgan:42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b",
@@ -64,24 +77,29 @@ def esrgan():
             exit(1)
         else:
             print(str(e))
-    print("Downloading...")
+    print(Fore.MAGENTA + "\t\tDownloading...")
     download(str(output), out_f, out_file_name, True)
 
 
 def sdxl():
-    prompt = input("Enter prompt for image: ")
-    out_file_name = input("Enter output file name: ")
+    prompt = input(Fore.BLUE + "\t\tEnter prompt for image: " + Fore.CYAN)
+    nPrompt = input(
+        Fore.BLUE + "\t\tEnter the things to be avoided in the image: " + Fore.CYAN)
+    if len(nPrompt) <= 0:
+        nPrompt = ""
+    out_file_name = input(
+        Fore.BLUE + "\t\tEnter output file name: " + Fore.CYAN)
     try:
-        width = int(input("Enter width: "))
-        height = int(input("Enter hieight: "))
+        width = int(input(Fore.BLUE + "\t\tEnter width: " + Fore.CYAN))
+        height = int(input(Fore.BLUE + "\t\tEnter hieight: " + Fore.CYAN))
     except ValueError:
-        width = 1024
-        height = 1024
-    print("Processing...")
+        width = 720
+        height = 720
+    print(Fore.BLUE + "\t\tProcessing...")
     try:
         output = replicate.run(
             "stability-ai/sdxl:8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f",
-            input={"prompt": f"{prompt}", "width": width,
+            input={"prompt": f"{prompt}", "negative_prompt": f"{nPrompt}", "width": width,
                    "height": height, "apply_watermark": False}
         )
     except Exception as e:
@@ -91,40 +109,51 @@ def sdxl():
             exit(1)
         else:
             print(str(e))
-    out_f = dir_path + f"\\{out_file_name}.jpeg"
-    print("Downloading...")
-    download(str(output[0]), out_f, out_file_name)
+    out_f = dir_path + f"\\Output\\{out_file_name}.jpeg"
+    print(Fore.MAGENTA + "\t\tDownloading...")
+    download(str(output[0]), out_f, out_file_name, False)
 
 
-def cartoonify():
-    file_path, out_f, out_file_name = file_path_and_check(True)
-    print("Processing...")
+def latent_consistency_model():
+    prompt = input(Fore.BLUE + "\t\tEnter prompt for image: " + Fore.CYAN)
+    out_file_name = input(
+        Fore.BLUE + "\t\tEnter output file name: " + Fore.CYAN)
+    try:
+        width = int(input(Fore.BLUE + "\t\tEnter width: " + Fore.CYAN))
+        height = int(input(Fore.BLUE + "\t\tEnter hieight: " + Fore.CYAN))
+    except ValueError:
+        width = 720
+        height = 720
+    print(Fore.BLUE + "\t\tProcessing...")
     try:
         output = replicate.run(
-            "catacolabs/cartoonify:043a7a0bb103cd8ce5c63e64161eae63a99f01028b83aa1e28e53a42d86191d3",
-            input={"image": open(file_path, "rb")}
+            "luosiallen/latent-consistency-model:553803fd018b3cf875a8bc774c99da9b33f36647badfd88a6eec90d61c5f62fc",
+            input={
+                "prompt": f"{prompt}", "width": width, "height": height}
         )
     except Exception as e:
         if "NSFW" in str(e):
-            print("NSFW Content Detected")
+            print("\t\tNSFW Content Detected")
             time.sleep(1)
             exit(1)
         else:
             print(str(e))
-    print("Downloading...")
-    download(str(output), out_f, out_file_name, True)
+    out_f = dir_path + f"\\Output\\{out_file_name}.png"
+    print(Fore.MAGENTA + "\t\tDownloading...")
+    download(str(output[0]), out_f, out_file_name, True)
 
 
 def anime_anything():
-    prompt = input("Enter prompt for image: ")
-    out_file_name = input("Enter output file name: ")
+    prompt = input(Fore.BLUE + "\t\tEnter prompt for image: " + Fore.CYAN)
+    out_file_name = input(
+        Fore.BLUE + "\t\tEnter output file name: " + Fore.CYAN)
     try:
-        width = int(input("Enter width: "))
-        height = int(input("Enter hieight: "))
+        width = int(input(Fore.BLUE + "\t\tEnter width: " + Fore.CYAN))
+        height = int(input(Fore.BLUE + "\t\tEnter hieight: " + Fore.CYAN))
     except ValueError:
         width = 512
         height = 512
-    print("Processing...")
+    print(Fore.BLUE + "\t\tProcessing...")
     try:
         output = replicate.run(
             "cjwbw/anything-v4.0:42a996d39a96aedc57b2e0aa8105dea39c9c89d9d266caf6bb4327a1c191b061",
@@ -132,40 +161,48 @@ def anime_anything():
         )
     except Exception as e:
         if "NSFW" in str(e):
-            print("NSFW Content Detected")
+            print(Fore.RED + "\t\tNSFW Content Detected")
             time.sleep(1)
             exit(1)
         else:
             print(str(e))
-    print("Downloading...")
+    print(Fore.MAGENTA + "\t\tDownloading...")
     out_f = dir_path + f"\\{out_file_name}.png"
     download(str(output[0]), out_f, out_file_name, True)
 
 
-if __name__ == "__main__":
+def loader():
     try:
         while True:
-            print("""\n
-                [1] Image upscaler (REAL ESRGAN)
-                [2] Text to image (SDXL)
-                [3] Cartoonify (CARTOONIFY)
-                [4] Anime (ANYTHING V4.0) -> MAX RES: [1024x768 or 768x1024]
-                [0] EXIT
+            print(Style.BRIGHT + Fore.RED + """\n\t\t
+                                        Image-SynthX \n""" +
+
+                  Fore.CYAN + """\n\t\t[1]""" + Fore.GREEN + """ Image upscaler""" + Fore.YELLOW + """ (REAL ESRGAN)""" +
+                  Fore.CYAN + """\n\t\t[2]""" + Fore.GREEN + """ Text to image""" + Fore.YELLOW + """ (SDXL)""" +
+                  Fore.CYAN + """\n\t\t[3]""" + Fore.GREEN + """ Text to image generation""" + Fore.YELLOW + """ (HIGH RES)(LATENT-CONSISTENCY-MODEL)""" +
+                  Fore.CYAN + """\n\t\t[4]""" + Fore.GREEN + """ Anime""" + Fore.YELLOW + """ (ANYTHING V4.0) -> MAX RES: [1024x768 or 768x1024]""" +
+                  Fore.CYAN + """\n\t\t[0]""" + Fore.GREEN + """ EXIT
             """)
-            choice = input("\nEnter choice: ")
+            choice = input(Fore.BLUE + Style.BRIGHT +
+                           "\n\t\tEnter choice: " + Fore.CYAN)
             if choice == "1":
                 esrgan()
             elif choice == "2":
                 sdxl()
             elif choice == "3":
-                cartoonify()
+                latent_consistency_model()
             elif choice == "4":
                 anime_anything()
             elif choice == "0":
+                print(Fore.RED + "\t\tExiting..")
+                time.sleep(1)
                 exit(0)
             else:
-                print("Invalid Choice\n")
+                print(Fore.RED + "\t\tInvalid Choice\n")
     except KeyboardInterrupt:
-        print("\nInterrupt Detected..")
+        print(Fore.RED + "\n\t\tInterrupt Detected..")
         time.sleep(1)
         exit(0)
+
+
+loader()
